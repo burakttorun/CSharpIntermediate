f791d6bb3a78140ead2ccb1dcb647d8396669216
Burak Torun <burakttorun@hotmail.com>
2020-12-29 22:32:44 +0300

    EntityFrameworkDemo project has been completed.

--- CSharpSolution/EntityFrameworkDemo/Form1.Designer.cs
+++ CSharpSolution/EntityFrameworkDemo/Form1.Designer.cs
@@ -59,6 +59,7 @@
             this.btnRemove.TabIndex = 7;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // gbxUpdate
             // 
@@ -84,6 +85,7 @@
             this.btnUpdate.TabIndex = 8;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // tbxStockAmountUpdate
             // 
@@ -157,6 +159,7 @@
             this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // tbxStockAmount
             // 
@@ -214,6 +217,7 @@
             this.dgwProducts.Name = "dgwProducts";
             this.dgwProducts.Size = new System.Drawing.Size(776, 226);
             this.dgwProducts.TabIndex = 4;
+            this.dgwProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwProducts_CellClick);
             // 
             // Form1
             // 
---
+++ CSharpSolution/EntityFrameworkDemo/ProductDal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.ToList(); //veri tabanındaki tabloda yer alan veriler forma aktarılır.
+
+            }
+        }
+
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product); //oluşturulan var entity contextle bağlantı kurup gönderilen parametreyi karşılaştırılır.
+                entity.State = System.Data.Entity.EntityState.Added;//EntityState de yer alan ekleme fonksiyonu çalıştırılır.
+                context.SaveChanges(); //değişiklikler kaydedilir.
+
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Modified;//EntityState de yer alan güncelleme fonksiyonu çalıştırılır.
+                context.SaveChanges();
+
+            }
+        }
+        public void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = System.Data.Entity.EntityState.Deleted;//EntityState de yer alan silme fonksiyonu çalıştırılır.
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
