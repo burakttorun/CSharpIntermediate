4c5b612ee4770937cb85e994e50d69c5b3c17cec
Burak Torun <burakttorun@hotmail.com>
2021-01-14 23:12:00 +0300

    Demo created about Events. :sparkles:

---
+++ CSharpSolution/Events/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void StockControl();
+    class Product
+    {
+        //belirlenen delegate tipinde bir even tanımlandı.
+        public event StockControl StockControlEvent;
+        private int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+       
+        public string ProductName { get; set; }
+       
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+
+
+            set
+            {
+                _stock = value;
+                //productın evente abone olup olmadığı kontrol edildi.
+                if (value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();//event çalıştırıldı.
+
+                }
+            }
+        }
+
+        public void SellProduct(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{0} amount: {1}",ProductName,Stock);
+        }
+
+
+    }
+}
---
+++ CSharpSolution/Events/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hardDisk = new Product(50);
+            hardDisk.ProductName = "Hard Disk";
+            Product mobilePhone = new Product(50);
+            mobilePhone.ProductName = "Mobile Phone";
+
+            // istenilen productın evente abone olması.
+            mobilePhone.StockControlEvent += MobilePhone_StockControlEvent;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                hardDisk.SellProduct(10);
+                mobilePhone.SellProduct(10);
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        //abone olan product için yapılacak olan eylem.
+        private static void MobilePhone_StockControlEvent()
+        {
+            Console.WriteLine("Mobile Phone amount is almost over!");
+        }
+    }
+}
